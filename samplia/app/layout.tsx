abb11ec7570607399962d232bb7ab44167b8c924
Álvaro <alvarofragosoc@hotmail.com>
2024-08-19 10:29:46 +0200

    changed title

--- samplia/app/layout.tsx
+++ samplia/app/layout.tsx
@@ -7,8 +7,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Samplia maps",
+  description: "Encuentra el reponedor más cercano y el estado de las máquinas",
 };
 
 export default function RootLayout({
